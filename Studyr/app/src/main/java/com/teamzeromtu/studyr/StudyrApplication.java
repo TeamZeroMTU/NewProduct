e742882c447381eef732821f261a78b944238e0e
Jeremy B. Daley <jbdaley@mtu.edu>
2016-03-22 18:05:40 -0400

    Adding a class that extends the application class. This can be used 	to store data that persists between activities. As a result, we 	need to pass only activity specific information with intents.

---
+++ Studyr/app/src/main/java/com/teamzeromtu/studyr/StudyrApplication.java
@@ -0,0 +1,10 @@
+package com.teamzeromtu.studyr;
+
+import android.app.Application;
+
+/**
+ * Created by jbdaley on 3/22/16.
+ */
+public class StudyrApplication extends Application {
+    public String userId = null;
+}
