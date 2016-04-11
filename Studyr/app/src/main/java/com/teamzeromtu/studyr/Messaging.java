0ca55cf21a87262ce380d3ed8d1d50bb5881e134
dsmith360 <derricks@mtu.edu>
2016-04-11 11:40:13 -0400

    fixed the transition from messaging to profile

--- Studyr/app/src/main/java/com/teamzeromtu/studyr/Messaging.java
+++ Studyr/app/src/main/java/com/teamzeromtu/studyr/Messaging.java
@@ -74,6 +74,8 @@ public class Messaging extends AppCompatActivity {
         NetworkTaskManager manager = app.taskManager;
         manager.execute( userMatches );
 
+
+
         if (matchList!=null)
             matches.setAdapter(new MessagingAdapter(matchList));
     }
@@ -90,6 +92,8 @@ public class Messaging extends AppCompatActivity {
     public void toProfile()
     {
         Intent change = new Intent(this, ProfileReadWrite.class);
+        StudyrApplication app = (StudyrApplication)getApplication();
+        change.putExtra(ProfileReadWrite.profileId, app.userId);
         startActivity(change);
         finish();
     }
